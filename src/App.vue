4fbacf9b1fe0c443f108ece78429bd7f18016415
Sampath  Gunasekara <wgsampath@gmail.com>
2021-08-02 12:13:45 +0530

    setup music app #mussic-app

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/music-app/Navbar.vue'
+import Home from './components/music-app/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/music-app/SearchResults.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container search-box">
+  <form class="d-flex">
+    <input class="form-control me-sm-2" type="text" placeholder="Search a Song">
+    <!-- <button class="my-2 btn btn-secondary my-sm-0" type="submit">Search</button> -->
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+}
+</script>
+
+<style scoped>
+.search-box{
+  margin-top:30px;
+  width: 50%;
+}
+</style>
